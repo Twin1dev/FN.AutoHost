250ba021e352db7e8e53da3b60a87154e0874c66
Twin1 <zaydinortiz@gmail.com>
2024-01-26 03:22:57 -0600

    WIP, NOT LAUNCHING YET.

---
+++ FN.AutoHost.V2/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FN.AutoHost.V2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void SetLogContent(string NewContent)
+        {
+            string OldContent = (string)LogLabel.Content;
+
+            LogLabel.Content = OldContent + "\n" + $"> {NewContent}";
+        }
+
+        public void SetLogContentBlank()
+        {
+            LogLabel.Content = "";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SetLogContent("Starting");
+            new Thread(new ThreadStart(() => AutoHost.V2.Fortnite.AutoHosting.Launch("")));
+          
+        }
+    }
+
+}
