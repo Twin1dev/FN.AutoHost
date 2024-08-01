684064db26f7181d607c56b879798351072b2796
Twin1 <64933300+Twin1dev@users.noreply.github.com>
2024-08-01 10:57:13 -0500

    new project and gui

---
+++ FN.AutoHost/Classes/Gui.cs
@@ -0,0 +1,157 @@
+﻿using ClickableTransparentOverlay;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImGuiNET;
+
+namespace FN.AutoHost.Classes
+{
+    public class Gui : Overlay
+    {
+        private int TAB = 1;
+
+        private void RenderMainTab()
+        {
+            ImGui.Text("Main");
+
+            if (ImGui.Button("Close"))
+            {
+                Environment.Exit(0);
+            }
+        }
+
+        private void RenderInfoTab()
+        {
+            ImGui.Text("Info");
+        }
+
+
+        private void RenderMainWindow()
+        {
+            ImGui.Begin("AutoHoster");
+            ImGui.SetWindowSize(new System.Numerics.Vector2(400, 400));
+
+            ImGui.BeginTabBar("WindowTabBar");
+
+
+            if (ImGui.TabItemButton("Main"))
+            {
+                TAB = 1;
+            }
+
+            if (ImGui.TabItemButton("Info"))
+            {
+                TAB = 2;
+            }
+
+            switch (TAB)
+            {
+                case 1:
+                    RenderMainTab();
+                    break;
+                case 2:
+                    RenderInfoTab();
+                    break;
+                default:
+                    break;
+            }
+
+            ImGui.EndTabBar();
+
+            ImGui.End();
+        }
+
+
+        private void SetStyle()
+        {
+            var colors = ImGui.GetStyle().Colors;
+            colors[(int)ImGuiCol.Text] = new(1.00f, 1.00f, 1.00f, 1.00f);
+            colors[(int)ImGuiCol.TextDisabled] = new(0.50f, 0.50f, 0.50f, 1.00f);
+            colors[(int)ImGuiCol.WindowBg] = new(0.10f, 0.10f, 0.10f, 1.00f);
+            colors[(int)ImGuiCol.ChildBg] = new(0.00f, 0.00f, 0.00f, 0.00f);
+            colors[(int)ImGuiCol.PopupBg] = new(0.19f, 0.19f, 0.19f, 0.92f);
+            colors[(int)ImGuiCol.Border] = new(0.19f, 0.19f, 0.19f, 0.29f);
+            colors[(int)ImGuiCol.BorderShadow] = new(0.00f, 0.00f, 0.00f, 0.24f);
+            colors[(int)ImGuiCol.FrameBg] = new(0.05f, 0.05f, 0.05f, 0.54f);
+            colors[(int)ImGuiCol.FrameBgHovered] = new(0.19f, 0.19f, 0.19f, 0.54f);
+            colors[(int)ImGuiCol.FrameBgActive] = new(0.20f, 0.22f, 0.23f, 1.00f);
+            colors[(int)ImGuiCol.TitleBg] = new(0.00f, 0.00f, 0.00f, 1.00f);
+            colors[(int)ImGuiCol.TitleBgActive] = new(0.06f, 0.06f, 0.06f, 1.00f);
+            colors[(int)ImGuiCol.TitleBgCollapsed] = new(0.00f, 0.00f, 0.00f, 1.00f);
+            colors[(int)ImGuiCol.MenuBarBg] = new(0.14f, 0.14f, 0.14f, 1.00f);
+            colors[(int)ImGuiCol.ScrollbarBg] = new(0.05f, 0.05f, 0.05f, 0.54f);
+            colors[(int)ImGuiCol.ScrollbarGrab] = new(0.34f, 0.34f, 0.34f, 0.54f);
+            colors[(int)ImGuiCol.ScrollbarGrabHovered] = new(0.40f, 0.40f, 0.40f, 0.54f);
+            colors[(int)ImGuiCol.ScrollbarGrabActive] = new(0.56f, 0.56f, 0.56f, 0.54f);
+            colors[(int)ImGuiCol.CheckMark] = new(0.33f, 0.67f, 0.86f, 1.00f);
+            colors[(int)ImGuiCol.SliderGrab] = new(0.34f, 0.34f, 0.34f, 0.54f);
+            colors[(int)ImGuiCol.SliderGrabActive] = new(0.56f, 0.56f, 0.56f, 0.54f);
+            colors[(int)ImGuiCol.Button] = new(0.05f, 0.05f, 0.05f, 0.54f);
+            colors[(int)ImGuiCol.ButtonHovered] = new(0.19f, 0.19f, 0.19f, 0.54f);
+            colors[(int)ImGuiCol.ButtonActive] = new(0.20f, 0.22f, 0.23f, 1.00f);
+            colors[(int)ImGuiCol.Header] = new(0.00f, 0.00f, 0.00f, 0.52f);
+            colors[(int)ImGuiCol.HeaderHovered] = new(0.00f, 0.00f, 0.00f, 0.36f);
+            colors[(int)ImGuiCol.HeaderActive] = new(0.20f, 0.22f, 0.23f, 0.33f);
+            colors[(int)ImGuiCol.Separator] = new(0.28f, 0.28f, 0.28f, 0.29f);
+            colors[(int)ImGuiCol.SeparatorHovered] = new(0.44f, 0.44f, 0.44f, 0.29f);
+            colors[(int)ImGuiCol.SeparatorActive] = new(0.40f, 0.44f, 0.47f, 1.00f);
+            colors[(int)ImGuiCol.ResizeGrip] = new(0.28f, 0.28f, 0.28f, 0.29f);
+            colors[(int)ImGuiCol.ResizeGripHovered] = new(0.44f, 0.44f, 0.44f, 0.29f);
+            colors[(int)ImGuiCol.ResizeGripActive] = new(0.40f, 0.44f, 0.47f, 1.00f);
+            colors[(int)ImGuiCol.Tab] = new(0.00f, 0.00f, 0.00f, 0.52f);
+            colors[(int)ImGuiCol.TabHovered] = new(0.14f, 0.14f, 0.14f, 1.00f);
+            colors[(int)ImGuiCol.TabActive] = new(0.20f, 0.20f, 0.20f, 0.36f);
+            colors[(int)ImGuiCol.TabUnfocused] = new(0.00f, 0.00f, 0.00f, 0.52f);
+            colors[(int)ImGuiCol.TabUnfocusedActive] = new(0.14f, 0.14f, 0.14f, 1.00f);
+            colors[(int)ImGuiCol.DockingPreview] = new(0.33f, 0.67f, 0.86f, 1.00f);
+            colors[(int)ImGuiCol.DockingEmptyBg] = new(1.00f, 0.00f, 0.00f, 1.00f);
+            colors[(int)ImGuiCol.PlotLines] = new(1.00f, 0.00f, 0.00f, 1.00f);
+            colors[(int)ImGuiCol.PlotLinesHovered] = new(1.00f, 0.00f, 0.00f, 1.00f);
+            colors[(int)ImGuiCol.PlotHistogram] = new(1.00f, 0.00f, 0.00f, 1.00f);
+            colors[(int)ImGuiCol.PlotHistogramHovered] = new(1.00f, 0.00f, 0.00f, 1.00f);
+            colors[(int)ImGuiCol.TableHeaderBg] = new(0.00f, 0.00f, 0.00f, 0.52f);
+            colors[(int)ImGuiCol.TableBorderStrong] = new(0.00f, 0.00f, 0.00f, 0.52f);
+            colors[(int)ImGuiCol.TableBorderLight] = new(0.28f, 0.28f, 0.28f, 0.29f);
+            colors[(int)ImGuiCol.TableRowBg] = new(0.00f, 0.00f, 0.00f, 0.00f);
+            colors[(int)ImGuiCol.TableRowBgAlt] = new(1.00f, 1.00f, 1.00f, 0.06f);
+            colors[(int)ImGuiCol.TextSelectedBg] = new(0.20f, 0.22f, 0.23f, 1.00f);
+            colors[(int)ImGuiCol.DragDropTarget] = new(0.33f, 0.67f, 0.86f, 1.00f);
+            colors[(int)ImGuiCol.NavHighlight] = new(1.00f, 0.00f, 0.00f, 1.00f);
+            colors[(int)ImGuiCol.NavWindowingHighlight] = new(1.00f, 0.00f, 0.00f, 0.70f);
+            colors[(int)ImGuiCol.NavWindowingDimBg] = new(1.00f, 0.00f, 0.00f, 0.20f);
+            colors[(int)ImGuiCol.ModalWindowDimBg] = new(1.00f, 0.00f, 0.00f, 0.35f);
+
+            var style = ImGui.GetStyle();
+            style.WindowPadding = new(8.00f, 8.00f);
+            style.FramePadding = new(5.00f, 2.00f);
+            style.CellPadding = new(6.00f, 6.00f);
+            style.ItemSpacing = new(6.00f, 6.00f);
+            style.ItemInnerSpacing = new(6.00f, 6.00f);
+            style.TouchExtraPadding = new(0.00f, 0.00f);
+            style.IndentSpacing = 25;
+            style.ScrollbarSize = 15;
+            style.GrabMinSize = 10;
+            style.WindowBorderSize = 1;
+            style.ChildBorderSize = 1;
+            style.PopupBorderSize = 1;
+            style.FrameBorderSize = 1;
+            style.TabBorderSize = 1;
+            style.WindowRounding = 7;
+            style.ChildRounding = 4;
+            style.FrameRounding = 3;
+            style.PopupRounding = 4;
+            style.ScrollbarRounding = 9;
+            style.GrabRounding = 3;
+            style.LogSliderDeadzone = 4;
+            style.TabRounding = 4;
+        }
+
+        protected override void Render()
+        {
+            SetStyle();
+            RenderMainWindow();
+        }
+    }
+}
--- FN.AutoHost/Program.cs
+++ FN.AutoHost/Program.cs
@@ -1,116 +1,16 @@
-﻿using FN.AutoHost.Properties;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using static FN.AutoHost.Win32;
-using IniParser;
-using IniParser.Model;
-using System.Windows.Forms;
+﻿using FN.AutoHost.Classes;
+using System.Runtime.InteropServices;
 
-namespace FN.AutoHost
-{
-    internal class Program
-    {
-        private delegate bool EventHandler(CtrlType sig);
-        static SetConsoleCtrlEventHandler _handler;
-        static void Main(string[] args)
-        {
-            // doesnt work at all for some odd reason : (
-         //   AppDomain.CurrentDomain.ProcessExit += ProcessExitHandler;
-            if (args.Length != 0) {
-                if (args.Contains("-change"))
-                {
-                    Console.WriteLine("What do you want to change the path to?");
-                    Console.Write(">> ");
-                    string path2 = Console.ReadLine();
-                    Settings.Default.path = path2;
-                    Settings.Default.Save();
-                    Console.WriteLine("Saved!");
-                } else if (args.Contains("-clear"))
-                {
-                    Settings.Default.Reset();
-                    Settings.Default.Save();
-                }
-            }
-            if (!File.Exists(Directory.GetCurrentDirectory() + "\\Settings.ini"))
-            {
-                // close to fix error
-                File.Create(Directory.GetCurrentDirectory() + "\\Settings.ini").Close();
-                var Parser = new FileIniDataParser();
-                IniData data = Parser.ReadFile(Directory.GetCurrentDirectory() + "\\Settings.ini");
-                data["Settings"]["Email"] = "server@projectreboot.dev";
-                data["Settings"]["Password"] = "Rebooted";
-                data["Settings"]["GameServerDllName"] = "Project Reboot 3.0.dll";
-                Parser.WriteFile(Directory.GetCurrentDirectory() + "\\Settings.ini", data);
-            }
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("[+] Started FN AutoHost");
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("[+] Starting Fortnite");
+[DllImport("kernel32.dll")]
+static extern IntPtr GetConsoleWindow();
 
-        
-        
+[DllImport("user32.dll")]
+static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
 
-            if (!Settings.Default.first)
-            {
-                Console.WriteLine("[-] Input Fortnite Path");
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.Write(">> ");
-                string path = Console.ReadLine();
-                if (!Directory.Exists(path))
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("[-] Entered Path Doesnt Exist! Exiting in 3 Seconds");
-                    Thread.Sleep(3000);
-                    Environment.Exit(0);
-                }
-            start:
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("It seems this is your first time using FN.AutoHost! Would you like to save this path? (you can change by opening 'change.bat'\n\n");
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("Yes/No");
-                Console.Write(">> ");
-                string output = Console.ReadLine();
+Gui gui = new Gui();
+Thread guiThread = new Thread(gui.Start().Wait);
+guiThread.Start();
 
-                
-                if (output.ToLower() != "yes" && output.ToLower() != "no")
-                {
-                    Console.WriteLine("Invalid response! Please respond with yes or no!");
-                    goto start;
-                }
+var handle = GetConsoleWindow();
 
-
-
-
-                if (output.ToLower() == "yes")
-                    Settings.Default.path = path;
-                else
-                    Console.WriteLine("Warning! this is unsupported!");
-                Settings.Default.first = true;
-                Settings.Default.Save();
-            }
-            Fortnite.Launch(Settings.Default.path);
-        }
-
-
-        static void ProcessExitHandler(object sender, EventArgs e)
-        {
-         
-            Console.WriteLine("Killing all fortnite processes...");
-            Fortnite.SafeKillProcess("FortniteClient-Win64-Shipping_BE");
-            Fortnite.SafeKillProcess("FortniteLauncher");
-            Fortnite.SafeKillProcess("FortniteClient-Win64-Shipping");
-            Fortnite.SafeKillProcess("CrashReportClient");
-            Console.WriteLine("Goodbye!");
-        }
-  
-    }
-
-  
-
-}
+ShowWindow(handle,0);
